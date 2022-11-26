c57322c7dd37c1031e9ddcf7a00244d3625be9d1
Adithyah Nair <adithyah.nair@gmail.com>
2022-11-26 10:19:06 +0530

    Added Drag Gesture with Animation

--- SwiftUI-Pinch/Screen/ContentView.swift
+++ SwiftUI-Pinch/Screen/ContentView.swift
@@ -12,6 +12,16 @@ struct ContentView: View {
 
     @State private var isAnimating: Bool = false
     @State private var imageScale: Double = 1
+    @State private var imageOffset: CGSize = .zero
+
+    // MARK: - Function
+
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
 
     var body: some View {
         NavigationView {
@@ -20,20 +30,36 @@ struct ContentView: View {
                     .resizable()
                     .scaledToFit()
                     .cornerRadius(10)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                     .shadow(color: .black, radius: 5, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
-                    .padding()
+                    .padding(20)
                     .animation(.linear(duration: 1), value: isAnimating)
                     .onTapGesture(count: 2) {
                         withAnimation(.spring()) {
                             if imageScale == 1 {
-                                imageScale = 5
+                                withAnimation(.spring()) {
+                                    imageScale = 5
+                                }
                             } else {
-                                imageScale = 1
+                                resetImageState()
                             }
                         }
                     }
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ gesture in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = gesture.translation
+                                }
+                            })
+                            .onEnded({ _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            })
+                    )
             }
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
