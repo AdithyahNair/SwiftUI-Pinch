b1f3740045a1ecb8c9af9c3de28bf88ed4ced81d
Adithyah Nair <adithyah.nair@gmail.com>
2022-12-02 07:30:29 +0530

    Added PageModel and PageData files.

---
+++ SwiftUI-Pinch/Data/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  SwiftUI-Pinch
+//
+//  Created by Adithyah Nair on 02/12/22.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+    Page(id: 1, imageName: "thumb-magazine-front-cover"),
+    Page(id: 2, imageName: "thumb-magazine-back-cover")
+]
---
+++ SwiftUI-Pinch/Model/PageModel.swift
@@ -0,0 +1,13 @@
+//
+//  PageModel.swift
+//  SwiftUI-Pinch
+//
+//  Created by Adithyah Nair on 02/12/22.
+//
+
+import Foundation
+
+struct Page: Identifiable {
+    let id: Int
+    let imageName: String
+}
